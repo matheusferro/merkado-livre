6405efc90b81308cac4c478b11f21417f671d6e5
matheusferro <matheussouzaferro@gmail.com>
2021-06-16 22:43:26 -0300

    category ajustes

--- src/main/kotlin/com/merkadoLivreGrpc/category/CategoryService.kt
+++ src/main/kotlin/com/merkadoLivreGrpc/category/CategoryService.kt
@@ -1,12 +1,12 @@
 package com.merkadoLivreGrpc.category
 
+import io.grpc.Status
 import io.micronaut.transaction.SynchronousTransactionManager
 import io.micronaut.validation.Validated
 import java.sql.Connection
 import javax.inject.Singleton
 import javax.persistence.EntityManager
 import javax.persistence.ParameterMode
-import javax.transaction.Transactional
 import javax.validation.Valid
 
 @Singleton
@@ -16,34 +16,38 @@ class CategoryService(
     private val transactionManager: SynchronousTransactionManager<Connection>
 ) {
 
-    @Transactional
-    fun save(@Valid categoryModel: CategoryModel) {
+    /**
+     * *Nasted sets*
+     *
+     * TODO: search
+     */
+    fun save(@Valid categoryModel: CategoryModel): String {
 
-        val parentCategory = //transactionManager.executeRead {
+        val parentCategory = transactionManager.executeRead {
             entityManager.createQuery(
                 "SELECT C FROM category C WHERE C.categoryName = :pName",
                 CategoryEntity::class.java
             )
                 .setParameter("pName", categoryModel.parent)
                 .resultList
-        //}
+        }
 
+        val result = transactionManager.executeWrite {
+            val sp = entityManager.createStoredProcedureQuery("nested_categoryfun")
+            sp.registerStoredProcedureParameter("cname", String::class.java, ParameterMode.IN)
+                .setParameter("cname", categoryModel.name)
+            sp.registerStoredProcedureParameter("pname", String::class.java, ParameterMode.IN)
+                .setParameter("pname", parentCategory[0].categoryName)
+            sp.registerStoredProcedureParameter("msg", String::class.java, ParameterMode.INOUT)
+                .setParameter("msg", "Cadastro com sucesso")
+            val result = sp.execute()
+            return@executeWrite result;
+        }
 
-        val sp = entityManager.createStoredProcedureQuery("nested_categoryfun")
-        sp.registerStoredProcedureParameter("cname", String::class.java, ParameterMode.IN)
-            .setParameter("cname", "teste")
-        sp.registerStoredProcedureParameter("pname", String::class.java, ParameterMode.IN)
-            .setParameter("pname", "categoryteste")
-        sp.registerStoredProcedureParameter("msg", String::class.java, ParameterMode.INOUT)
-            .setParameter("msg", "teste")
-        sp.execute()
-//        transactionManager.executeWrite {
-//            entityManager.createNativeQuery("call nested_category(:pCname, :pPname)")
-//                .setParameter("pCname", "teste")
-//                .setParameter("pPname","categoryteste")
-//                .
-//        }
-        return
+        return when {
+            result -> "Cadastrado!";
+            else -> throw Status.UNAVAILABLE.withDescription("Não foi possível realizar o cadastro").asException()
+        }
     }
 }
 
